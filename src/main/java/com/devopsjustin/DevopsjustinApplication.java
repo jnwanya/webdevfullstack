96c386a7316a3a0369b02aa44d3887ba02514317
Nwanya Justin <nwanyajustin@gmail.com>
2017-12-25 18:56:29 +0100

    Added spring starter project files

---
+++ src/main/java/com/devopsjustin/DevopsjustinApplication.java
@@ -0,0 +1,12 @@
+package com.devopsjustin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsjustinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsjustinApplication.class, args);
+	}
+}
