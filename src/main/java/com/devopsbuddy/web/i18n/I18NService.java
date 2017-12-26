413490c393db2de2d543e989d27804754a0bf24b
Nwanya Justin <nwanyajustin@gmail.com>
2017-12-26 17:22:56 +0100

    Added logger xml file

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.devopsbuddy.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -14,6 +16,7 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
     @Autowired
     private MessageSource messageSource;
 
@@ -22,6 +25,7 @@ public class I18NService {
      * @param messageId the key to the messages resource file
      */
     public String getMessage(String messageId){
+        LOG.info("Returning i18n message for {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
