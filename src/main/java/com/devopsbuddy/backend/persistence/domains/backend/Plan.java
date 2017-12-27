9e3cef28e4b0d5f73423dff1906a9a53c8b9d412
Nwanya Justin <nwanyajustin@gmail.com>
2017-12-27 18:58:33 +0100

    Created JPA models for spring security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domains/backend/Plan.java
@@ -0,0 +1,56 @@
+package com.devopsbuddy.backend.persistence.domains.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by Jnwanya on
+ * Wed, 27 Dec, 2017.
+ */
+@Entity
+public class Plan implements Serializable {
+    /** The serialversionUID for a serializabled class **/
+    private static final long serialVersionUID = 1L;
+    @Id
+    private int id;
+
+    private String name;
+
+    /** Default constructor **/
+    public Plan(){ }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Plan plan = (Plan) o;
+
+        if (id != plan.id) return false;
+        return name.equals(plan.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
---
+++ src/main/java/com/devopsbuddy/backend/persistence/domains/backend/Role.java
@@ -0,0 +1,67 @@
+package com.devopsbuddy.backend.persistence.domains.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Jnwanya on
+ * Wed, 27 Dec, 2017.
+ */
+@Entity
+public class Role implements Serializable {
+    /** The serialversionUID for a serializabled class **/
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+
+    public Role(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
